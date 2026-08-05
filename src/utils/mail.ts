c10d53dc07866d93e02a76580f4850858c50f5d0
Saw Su Han <sawsuhan2003@gmail.com>
2026-08-05 14:39:05 +0630

    feat:update mail

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,10 +1,17 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import dns from "node:dns";
+
 dotenv.config();
+
+dns.setDefaultResultOrder("ipv4first");
+
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER!,
+    pass: process.env.EMAIL_PASS!,
   },
 });
