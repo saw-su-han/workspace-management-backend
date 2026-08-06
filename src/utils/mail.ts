ddd2183fda19adee43400846735b9408ba2608ac
Saw Su Han <sawsuhan2003@gmail.com>
2026-08-06 09:04:15 +0630

    update port

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -6,9 +6,8 @@ dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: Number(process.env.EMAIL_PORT || 587),
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
